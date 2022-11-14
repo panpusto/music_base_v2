f6965fd96d5d5b84e96423d9d1addbc56076a425
Cezary <102999499+panpusto@users.noreply.github.com>
2022-11-14 15:29:37 +0100

    update with allauth paths

--- django_project/urls.py
+++ django_project/urls.py
@@ -22,8 +22,7 @@ urlpatterns = [
     # admin
     path('admin/', admin.site.urls),
     # user management
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')),
     # local apps
-    path('accounts/', include('accounts.urls')),
     path('', include('pages.urls')),
 ]
