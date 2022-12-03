628e2baa91275fc4915ff1017c839ded50db47da
Cezary <102999499+panpusto@users.noreply.github.com>
2022-12-04 00:15:47 +0100

    genre list view

--- genres/views.py
+++ genres/views.py
@@ -1,8 +1,18 @@
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views.generic import CreateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import GenreCreationForm
+from .models import Genre
 
-class GenreCreateView(generic.CreateView):
+
+class GenreCreateView(LoginRequiredMixin, CreateView):
     form_class = GenreCreationForm
     success_url = reverse_lazy('home')
     template_name = 'genres/create_form.html'
+
+
+class GenreListView(ListView):
+    model = Genre
+    context_object_name = 'genre_list'
+    template_name = 'genres/genre_list.html'
+    ordering = 'name'
