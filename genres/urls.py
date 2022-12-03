a732c683f9707faafc8779e922509a7203e54b9b
Cezary <102999499+panpusto@users.noreply.github.com>
2022-12-04 00:14:40 +0100

    genre list path

--- genres/urls.py
+++ genres/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
-from .views import GenreCreateView
+from .views import GenreCreateView, GenreListView
 
 urlpatterns = [
+    path('/', GenreListView.as_view(), name='genre_list'),
     path('add/', GenreCreateView.as_view(), name='add_genre'),
 ]
