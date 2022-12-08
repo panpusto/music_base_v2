66f4e6b95eca278257a66b582fdfea8b134d1e8f
Cezary <102999499+panpusto@users.noreply.github.com>
2022-12-08 15:18:44 +0100

    add label detail view

--- labels/urls.py
+++ labels/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LabelCreateView, LabelListView
+from .views import LabelCreateView, LabelListView, LabelDetailView
 
 urlpatterns = [
     path('', LabelListView.as_view(), name='label_list'),
+    path('<int:pk>/', LabelDetailView.as_view(), name='label_detail'),
     path('add/', LabelCreateView.as_view(), name='add_label'),
 ]
--- labels/views.py
+++ labels/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView
 from labels.forms import LabelCreationForm
 from django.urls import reverse_lazy
 from labels.models import Label
@@ -8,6 +8,7 @@ class LabelCreateView(LoginRequiredMixin, CreateView):
     form_class = LabelCreationForm
     success_url = reverse_lazy('home')
     template_name = 'labels/create_form.html'
+    login_url = 'account_login'
 
     def form_valid(self, form):
         form.instance.added_by = self.request.user
@@ -20,3 +21,8 @@ class LabelListView(ListView):
     template_name = 'labels/label_list.html'
     ordering = 'name'
     
+
+class LabelDetailView(DetailView):
+    model = Label
+    context_object_name = 'label'
+    template_name = 'labels/label_detail.html'
