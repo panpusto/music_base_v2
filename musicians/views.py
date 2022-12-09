65dbc71846b23ab133f0d7949fce92a766956216
Cezary <102999499+panpusto@users.noreply.github.com>
2022-12-09 14:34:16 +0100

    views for create, list and detail

--- musicians/views.py
+++ musicians/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, ListView, DetailView
+from musicians.forms import MusicianCreationForm
+from django.urls import reverse_lazy
+from musicians.models import Musician
 
-# Create your views here.
+
+class MusicianCreateView(LoginRequiredMixin, CreateView):
+    form_class = MusicianCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'musicians/create_form.html'
+    login_url = 'account_login'
+
+    def form_valid(self, form):
+        form.instance.added_by = self.request.user
+        return super().form_valid(form)
+        
+
+class MusicianListView(ListView):
+    model = Musician
+    context_object_name = 'musician_list'
+    template_name = 'musicians/musician_list.html'
+    ordering = 'name'
+
+
+class MusicianDetailView(DetailView):
+    model = Musician
+    context_object_name = 'musician'
+    template_name = 'musicians/musician_detail.html'
