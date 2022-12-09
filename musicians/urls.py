03a162180342a5117b369b8319e8200b50e7c843
Cezary <102999499+panpusto@users.noreply.github.com>
2022-12-09 14:37:07 +0100

    add paths for musicians views

---
+++ musicians/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from musicians.views import MusicianListView, MusicianCreateView, MusicianDetailView
+
+
+urlpatterns = [
+    path('', MusicianListView.as_view(), name='musician_list'),
+    path('add/', MusicianCreateView.as_view(), name='add_musician'),
+    path('<int:pk>/', MusicianDetailView.as_view(), name='musician_detail'),
+]
