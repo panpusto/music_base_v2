1482835752d0b275cd9f256fc9313fe6d88826ac
Cezary <102999499+panpusto@users.noreply.github.com>
2022-12-11 20:55:23 +0100

    add create and list views

--- bands/views.py
+++ bands/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, ListView
+from bands.forms import BandCreationForm
+from django.urls import reverse_lazy
+from bands.models import Band
 
-# Create your views here.
+
+class BandCreateView(LoginRequiredMixin, CreateView):
+    form_class = BandCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'bands/create_form.html'
+    login_url = 'account_login'
+
+    def form_valid(self, form):
+        form.instance.added_by = self.request.user
+        return super().form_valid(form)
+
+
+class BandListViewAlphabetically(ListView):
+    model = Band
+    context_object_name = 'band_list'
+    template_name = 'bands/band_list_alphabetically.html'
+    ordering = 'name'
