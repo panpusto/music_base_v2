eb054089c06610b5b0a5bffdaa7963b8b1885a8d
Cezary <102999499+panpusto@users.noreply.github.com>
2022-12-11 20:20:09 +0100

    register model for admin site

--- bands/admin.py
+++ bands/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from bands.models import Band
 
-# Register your models here.
+
+admin.site.register(Band)
--- musiciansbands/admin.py
+++ musiciansbands/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from musiciansbands.models import MusicianBand
 
-# Register your models here.
+
+admin.site.register(MusicianBand)
