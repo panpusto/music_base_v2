544c1222110d333e7e2e55a0fc650d0db0db52ac
Cezary <102999499+panpusto@users.noreply.github.com>
2022-12-16 14:22:36 +0100

    add create view

--- musiciansbands/views.py
+++ musiciansbands/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
+from musiciansbands.forms import MusicianToBandCreationForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class MusicianToBandCreateView(LoginRequiredMixin, CreateView):
+    form_class = MusicianToBandCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'musiciansbands/create_form.html'
+    login_url = 'account_login'
+
+    def form_valid(self, form):
+        form.instance.added_by = self.request.user
+        return super().form_valid(form)
+        
