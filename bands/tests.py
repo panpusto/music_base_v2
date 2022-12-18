999538b57f3db5bfdd57af2426d07279a3b7af3f
Cezary <102999499+panpusto@users.noreply.github.com>
2022-12-18 14:26:56 +0100

    add tests for bands app

--- bands/tests.py
+++ bands/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from bands.models import Band
+from labels.models import Label
+from genres.models import Genre
 from django.urls import reverse
 
 
@@ -14,6 +16,15 @@ class BandTests(TestCase):
             email='testuser1@email.com',
         )
 
+        cls.label = Label.objects.create(
+            name='2020',
+            address='Warsaw',
+            country='Poland',
+            status=1,
+            founding_year=2020,
+            added_by=cls.user
+        )
+
         cls.band = Band.objects.create(
             name='Band',
             country_of_origin='USA',
@@ -21,9 +32,8 @@ class BandTests(TestCase):
             status=1,
             formed_in=2000,
             ended_in=2000,
-            genre=[1],
             lyrical_themes='themes',
-            current_label=1,
+            current_label=cls.label,
             bio='bio',
             added_by=cls.user
         )
@@ -33,33 +43,30 @@ class BandTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(f'{self.band.name}', 'Band')
 
-    # def test_add_musician_view_for_logged_in_user(self):
-    #     self.client.login(email='testuser1@email.com', password='testpass123')
-    #     response = self.client.get(reverse('add_musician'))
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertContains(response, 'Add new musician')
-    #     self.assertNotContains(response, 'Not contain me')
-    #     self.assertTemplateUsed(response, 'musicians/create_form.html')
+    def test_add_band_view_for_logged_in_user(self):
+        self.client.login(email='testuser1@email.com', password='testpass123')
+        response = self.client.get(reverse('add_band'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Add new band')
+        self.assertNotContains(response, 'Not contain me')
+        self.assertTemplateUsed(response, 'bands/create_form.html')
 
-    # def test_add_musician_view_for_logged_out_user(self):
-    #     self.client.logout()
-    #     response = self.client.get(reverse('add_musician'))
-    #     self.assertEqual(response.status_code, 302)
-    #     self.assertRedirects(
-    #         response, f'{reverse("account_login")}?next=/musicians/add/'
-    #     )
-    #     response = self.client.get(
-    #         f'{reverse("account_login")}?next=/musicians/add/'
-    #     )
-    #     self.assertContains(response, 'Log In')
+    def test_add_band_view_for_logged_out_user(self):
+        self.client.logout()
+        response = self.client.get(reverse('add_band'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response, f'{reverse("account_login")}?next=/bands/add/'
+        )
+        response = self.client.get(
+            f'{reverse("account_login")}?next=/bands/add/'
+        )
+        self.assertContains(response, 'Log In')
 
-    # def test_musician_detail_view(self):
-    #     response = self.client.get(self.musician.get_absolute_url())
-    #     no_response = self.client.get('musicians/123456/')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(no_response.status_code, 404)
-    #     self.assertContains(response, 'info about musician')
-    #     self.assertTemplateUsed(response, 'musicians/musician_detail.html')
-
-    # TODO:
-    # - add label instance
+    def test_band_detail_view(self):
+        response = self.client.get(self.band.get_absolute_url())
+        no_response = self.client.get('bands/123456/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'Breslau')
+        self.assertTemplateUsed(response, 'bands/band_detail.html')
