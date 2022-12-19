29f3a21b50c04ac6f72dbce4ad19a34e99cadefc
Cezary <102999499+panpusto@users.noreply.github.com>
2022-12-19 14:31:10 +0100

    add path for detail view

--- albums/urls.py
+++ albums/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from albums.views import AlbumListViewAlphabetically
+from albums.views import AlbumListViewAlphabetically, AlbumDetailView
 
 
 urlpatterns = [
     path('', AlbumListViewAlphabetically.as_view(), name='album_list_alphabetically'),
+    path('<int:pk>/', AlbumDetailView.as_view(), name='album_detail'),
 ]
