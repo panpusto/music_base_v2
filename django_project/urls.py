0faf23ea5b4509f4558046cae1261025f94f1741
Cezary <102999499+panpusto@users.noreply.github.com>
2022-12-19 13:46:15 +0100

    add static

--- django_project/urls.py
+++ django_project/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
 
 urlpatterns = [
     # debug
@@ -30,4 +33,6 @@ urlpatterns = [
     path('musicians/', include('musicians.urls')),
     path('bands/', include('bands.urls')),
     path('musiciantoband/', include('musiciansbands.urls')),
-]
+] + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
