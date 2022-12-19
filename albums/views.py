b6d4f6919b1a01285121035279b29da678f52bad
Cezary <102999499+panpusto@users.noreply.github.com>
2022-12-19 14:31:32 +0100

    add detail view

--- albums/views.py
+++ albums/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from albums.models import Album
 
 
@@ -7,3 +7,9 @@ class AlbumListViewAlphabetically(ListView):
     context_object_name = 'album_list'
     template_name = 'albums/album_list_alphabetically.html'
     ordering = 'title'
+
+
+class AlbumDetailView(DetailView):
+    model = Album
+    context_object_name = 'album'
+    template_name = 'albums/album_detail.html'
