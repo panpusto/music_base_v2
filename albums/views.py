1557dbc51aaf2cf8e0b775099c444bfb9dcfc1af
Cezary <102999499+panpusto@users.noreply.github.com>
2022-12-19 14:07:21 +0100

    add list view for albums

--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from albums.models import Album
 
-# Create your views here.
+
+class AlbumListViewAlphabetically(ListView):
+    model = Album
+    context_object_name = 'album_list'
+    template_name = 'albums/album_list_alphabetically.html'
+    ordering = 'title'
