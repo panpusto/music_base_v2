e1ba62077ba0e0fc8103de70248fd977b3b810cc
Cezary <102999499+panpusto@users.noreply.github.com>
2022-12-20 13:34:48 +0100

    add path for adding album form

--- albums/urls.py
+++ albums/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from albums.views import AlbumListViewAlphabetically, AlbumDetailView
+from albums.views import AlbumListViewAlphabetically, AlbumDetailView, AlbumCreateView
 
 
 urlpatterns = [
     path('', AlbumListViewAlphabetically.as_view(), name='album_list_alphabetically'),
     path('<int:pk>/', AlbumDetailView.as_view(), name='album_detail'),
+    path('add/', AlbumCreateView.as_view(), name='add_album'),
 ]
