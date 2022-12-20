5d52b702d4e83a4929328d2935e0826621143c25
Cezary <102999499+panpusto@users.noreply.github.com>
2022-12-20 13:35:15 +0100

    add create view for album

--- albums/views.py
+++ albums/views.py
@@ -1,5 +1,15 @@
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from albums.forms import AlbumCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 from albums.models import Album
+
+
+class AlbumCreateView(LoginRequiredMixin, CreateView):
+    form_class = AlbumCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'albums/create_form.html'
+    login_url = 'account_login'
 
 
 class AlbumListViewAlphabetically(ListView):
