c01c6a700280d608ff96f4275e0fbfc64b21215b
Cezary <102999499+panpusto@users.noreply.github.com>
2022-12-22 13:31:34 +0100

    add path for list and detail views

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from reviews.views import ReviewCreateView
+from reviews.views import ReviewCreateView, ReviewListView, ReviewDetailView
 
 
 urlpatterns = [
+    path('', ReviewListView.as_view(), name='review_list'),
+    path('<int:pk>/', ReviewDetailView.as_view(), name='review_detail'),
     path('add/', ReviewCreateView.as_view(), name='add_review'),
 ]
