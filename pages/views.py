19a9684ccc2ff61002eef2887cfdac3c8277df83
Cezary <102999499+panpusto@users.noreply.github.com>
2023-01-03 20:56:37 +0100

    add band/artist search

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,19 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from bands.models import Band
+from django.db.models import Q
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+class SearchResultsListView(ListView):
+    model = Band
+    context_object_name = 'search_results'
+    template_name = 'search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET['searching_query']
+        return Band.objects.filter(
+            Q(name__icontains=query) | Q(members__name__icontains=query)
+        )
