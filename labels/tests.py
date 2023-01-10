f8987418fa02bcf711076a2806f25312829dca3d
Cezary <102999499+panpusto@users.noreply.github.com>
2023-01-10 15:15:31 +0100

    add tests for update view for labels

--- labels/tests.py
+++ labels/tests.py
@@ -20,7 +20,8 @@ class LabelTests(TestCase):
             country='Poland',
             status=1,
             founding_year=2020,
-            added_by=cls.user
+            added_by=cls.user,
+            styles='rock'
         )
 
     def test_label_listing(self):
@@ -29,7 +30,9 @@ class LabelTests(TestCase):
         self.assertEqual(f'{self.label.name}', '2020') 
 
     def test_add_label_view_for_logged_in_user(self):
-        self.client.login(email='testuser1@email.com', password='testpass123')
+        self.client.login(
+            email='testuser1@email.com',
+            password='testpass123')
         response = self.client.get(reverse('add_label'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Add new label')
@@ -41,7 +44,8 @@ class LabelTests(TestCase):
         response = self.client.get(reverse('add_label'))
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(
-            response, f'{reverse("account_login")}?next=/labels/add/'
+            response,
+            f'{reverse("account_login")}?next=/labels/add/'
         )
         response = self.client.get(
             f'{reverse("account_login")}?next=/labels/add/'
@@ -55,3 +59,46 @@ class LabelTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'Warsaw')
         self.assertTemplateUsed(response, 'labels/label_detail.html')
+
+    def test_label_update_view_for_logged_in_user(self):
+        self.client.login(
+            email='testuser1@email.com',
+            password='testpass123'
+        )
+        response = self.client.post(
+            reverse(
+                'label_update',
+                kwargs={'pk': self.label.id}),
+                {
+                    'name': 'Terratur',
+                    'address': 'Warsaw',
+                    'country': 'Poland',
+                    'status': 1,
+                    'founding_year': 2020,
+                    'added_by': self.user.id,
+                    'styles': 'rock'
+                }
+        )
+        self.assertEqual(response.status_code, 302)
+        self.label.refresh_from_db()
+        response = self.client.get(self.label.get_absolute_url())
+        self.assertContains(response, 'Terratur')
+
+    def test_label_update_view_for_logged_out_user(self):
+        self.client.logout()
+        response = self.client.get(
+            reverse(
+                'label_update',
+                kwargs={'pk': self.label.id}
+            )
+        )  
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response,
+            f'{reverse("account_login")}?next=/labels/update/{self.label.id}/'
+        )
+        response = self.client.get(
+            f'{reverse("account_login")}?next=/labels/update/{self.label.id}/'
+        )
+        self.assertContains(response, 'Log In')
+        
