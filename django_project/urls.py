bdb7f747bb6ee2c00fb3d6adcf5ca993f8bf7b14
Cezary <102999499+panpusto@users.noreply.github.com>
2023-01-24 14:14:09 +0100

    add schema and documentation paths

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,23 +1,12 @@
-"""django_project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView
+)
 
 urlpatterns = [
     # debug
@@ -29,6 +18,12 @@ urlpatterns = [
     # api
     path('api/v1/', include('apis.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    #schema
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path("api/schema/redoc/", SpectacularRedocView.as_view(
+        url_name="schema"), name="redoc",),
+    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(
+        url_name="schema"), name="swagger-ui"),
     # local apps
     path('', include('pages.urls')),
     path('genres/', include('genres.urls')),
