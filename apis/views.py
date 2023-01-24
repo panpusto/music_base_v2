e3a11e01a0b4430362e557e4cc3a9c0804cf05f5
Cezary <102999499+panpusto@users.noreply.github.com>
2023-01-24 12:07:39 +0100

    add views description

--- apis/views.py
+++ apis/views.py
@@ -15,46 +15,58 @@ from .serializers import (
 
 
 class AlbumListAPIView(g.ListAPIView):
+    """Displays list of albums in json format."""
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
 
 
 class AlbumDetailAPIView(g.RetrieveAPIView):
+    """Displays a single album details in json format."""
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
 
 
 class BandListAPIView(g.ListAPIView):
+    """Displays list of bands in json format."""
     queryset = Band.objects.all()
     serializer_class = BandSerializer
 
 
 class BandDetailAPIView(g.RetrieveAPIView):
+    """Displays a single band details in json format."""
     queryset = Band.objects.all()
     serializer_class = BandSerializer
 
 
 class LabelListAPIView(g.ListAPIView):
+    """Displays list of labels in json format."""
     queryset = Label.objects.all()
     serializer_class = LabelSerializer
 
 
 class LabelDetailAPIView(g.RetrieveAPIView):
+    """Displays a single label details in json format."""
     queryset = Label.objects.all()
     serializer_class = LabelSerializer
 
 
 class MusicianListAPIView(g.ListAPIView):
+    """Displays list of musicians in json format."""
     queryset = Musician.objects.all()
     serializer_class = MusicianSerializer
 
 
 class MusicianDetailAPIView(g.RetrieveAPIView):
+    """Displays a single musician details in json format."""
     queryset = Musician.objects.all()
     serializer_class = MusicianSerializer
 
 
 class UserListAPIView(g.ListAPIView):
+    """
+    Displays list of users in json format.
+    Only for admins.
+    """
     permission_classes = (permissions.IsAdminUser,)
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
